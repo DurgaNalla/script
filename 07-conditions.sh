cfc141d95e4c26425b8ba2a503e712e0a68e8d5e
DurgaNalla <durganaganalla555@gmail.com>
2024-11-09 18:36:20 +0530

    hello script

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -2,13 +2,9 @@
 
 NUMBER=$1
 
-if [$NUMBER -gt 10]
- then
-   {
-     echo " $NUMBER is GREATER"
-   }
-   else
-   {
-     echo "$NUMBER is LESSER"
-   }
-   fi
+if [ $NUMBER -gt 10 ]
+then
+   echo " $NUMBER is GREATER"
+else
+   echo "$NUMBER is LESSER"
+fi
