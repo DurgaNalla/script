18abe6f80cbd0063de69dad0dfc1f2f07110bcb1
DurgaNalla <durganaganalla555@gmail.com>
2024-11-09 14:01:45 +0530

    hello script

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-person1=Jyo
-person2=devi
+person1=$1
+person2=$2
  echo "$person1: hi $person2 how are you"
  echo "$person2 : iam fine .how are you?"
  echo "$person1: iam also fine .how is your studies"
