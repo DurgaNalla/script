07edea9d982b319fc1c42a3ef38a4d8c4b68f72b
DurgaNalla <durganaganalla555@gmail.com>
2024-11-09 13:39:00 +0530

    hello script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ 
+ echo "Devi: hi jyo how are you"
+ echo "Jyo : iam fine .how are you?"
+ echo "Devi: iam also fine .how is your studies"
+ echo "jyo : not bad this year my graduation completed "
